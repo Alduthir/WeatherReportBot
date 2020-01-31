ac6fc21c8287d304168dbc6e8ca46c280c87bcbc
Peter Askey <peter_g_askey@live.co.uk>
2020-01-31 12:50:06 +0100

    secured bot token

--- src/main/java/com/weatherreport/Bot.java
+++ src/main/java/com/weatherreport/Bot.java
@@ -4,18 +4,16 @@ import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Channel;
 import discord4j.core.object.entity.MessageChannel;
 import discord4j.core.object.entity.User;
 import reactor.core.publisher.Mono;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 final public class Bot {
-    /**
-     * Discord bot token
-     */
-    private static final String token = "NjcxNjMzNzA5ODM3MDU4MDQ4.XjPnIg.NthU5XOMJY1iRpt4D1CXlbI6oGs";
 
     /**
      * Prefix character the bot should listen to
@@ -29,14 +27,30 @@ final public class Bot {
 
     private static final WeatherReport weatherReport = new WeatherReport();
 
+    /* Fills the commands Map with keys and their corresponding command. */
+    static {
+        commands.put("ping", event -> event.getMessage()
+                .getChannel().block()
+                .createMessage("Pong!").block());
+
+        commands.put("report", event -> {
+            final String content = event.getMessage().getContent().get();
+            final List<String> command = Arrays.asList(content.split(" "));
+            final Mono<MessageChannel> channel = event.getMessage().getChannel();
+
+            if (command.size() > 1) {
+                channel.block().createMessage((weatherReport.getWeatherReport(command.get(1)))).block();
+            } else {
+                channel.block().createMessage("Please specify a location for me to check the weather at.").block();
+            }
+        });
+    }
+
     /**
      * @param args args
      */
     public static void main(String[] args) {
-        Map<String, Command> commandList = new HashMap<>();
-        initCommands();
-
-        DiscordClientBuilder builder = new DiscordClientBuilder(token);
+        DiscordClientBuilder builder = new DiscordClientBuilder(System.getenv("BOT_TOKEN"));
         DiscordClient instance = builder.build();
         login(instance);
 
@@ -64,24 +78,5 @@ final public class Bot {
                 });
     }
 
-    /**
-     * Fills the commands Map with keys and their corresponding command.
-     */
-    private static void initCommands() {
-        commands.put("ping", event -> Objects.requireNonNull(event.getMessage()
-                .getChannel().block())
-                .createMessage("Pong!").block());
 
-        commands.put("report", event -> {
-            final String content = event.getMessage().getContent().get();
-            final List<String> command = Arrays.asList(content.split(" "));
-            final Mono<MessageChannel> channel = event.getMessage().getChannel();
-
-            if (command.size() > 1) {
-                Objects.requireNonNull(channel.block()).createMessage((weatherReport.getWeatherReport(command.get(1)))).block();
-            } else {
-                Objects.requireNonNull(channel.block()).createMessage("Please specify a location for me to check the weather at.").block();
-            }
-        });
-    }
 }
